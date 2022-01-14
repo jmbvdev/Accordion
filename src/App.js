1426dc0cab8c53c97558b76d5090296add6cc177
jmbvdev <jmvb.dev@gmail.com>
2022-01-14 17:39:33 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import data from "./data"
 import './App.css';
+import Questions from "./Questions";
 
-function App() {
+
+function App() { 
+
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {
+            data.map((question)=>{
+              return <Questions key={question.id} {...question}/>
+
+            })
+          }
+        </section>
+      </div>
+    </main>
+   
   );
 }
 
